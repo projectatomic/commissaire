f225fa550c0c969786fbf205134d694ee0901b2b
Steve Milner <smilner@redhat.com>
2016-01-07 14:27:12 -0500

    Added initial setup.py.

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+###
+# Copyright (C) 2016  Red Hat, Inc
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Source build and installation script.
+"""
+
+from setuptools import setup, find_packages
+
+
+def extract_requirements(filename):
+    with open(filename, 'r') as requirements_file:
+        return [x[:-1] for x in requirements_file.readlines()]
+
+
+install_requires = extract_requirements('requirements.txt')
+test_require = extract_requirements('test-requirements.txt')
+
+
+setup(
+    name='commissaire',
+    version='0.0.0',
+    description='Simple cluster management',
+    author='Steve Milner',
+    url='https://github.com/projectatomic/commissaire',
+    license="AGPLv3+",
+
+    install_requires=install_requires,
+    tests_require=test_require,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    entry_points={
+        'console_scripts': [
+            'commissaire = commissaire.script:main',
+        ],
+    }
+)
