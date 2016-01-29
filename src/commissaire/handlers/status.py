ashcrow/tbd

Added clusterexec greenlet to status.

src/commissaire/handlers/status.py
@@ -30,7 +30,7 @@ class Status(Model):
     """
     _json_type = dict
     _attributes = (
-        'etcd', 'investigator')
+        'etcd', 'investigator', 'clusterexec')
 
 
 class StatusResource(Resource):
@@ -58,6 +58,14 @@ class StatusResource(Resource):
                     'size': 0,
                     'in_use': 0,
                     'errors': [],
+                },
+            },
+            'clusterexec': {
+                'status': 'FAILED',
+                'info': {
+                    'size': 0,
+                    'in_use': 0,
+                    'errors': [],
                 }
             },
         }
@@ -70,21 +78,27 @@ class StatusResource(Resource):
         except etcd.EtcdKeyNotFound:
             kwargs['etcd']['status'] = 'FAILED'
 
-        # Check the investigator pool
-        kwargs['investigator']['info']['size'] = POOLS['investigator'].size
-        kwargs['investigator']['info']['in_use'] = (
-            POOLS['investigator'].size - POOLS['investigator'].free_count())
+        # Check all the pools
+        def populate_pool_info(pool):
+            # Append the pool information
+            kwargs[pool]['info']['size'] = POOLS[pool].size
+            kwargs[pool]['info']['in_use'] = (
+                POOLS[pool].size - POOLS[pool].free_count())
 
-        exceptions = False
-        for thread in POOLS['investigator'].greenlets:
-            if thread.exception:
-                exceptions = True
-                POOLS['investigator']['info']['errors'].append(
-                    thread.exception)
+        map(populate_pool_info, POOLS.keys())
 
-        if POOLS['investigator'].free_count() == 0:
+        def populate_exceptions(pool):
+            exceptions = False
+            for thread in POOLS[pool].greenlets:
+                if thread.exception:
+                    exceptions = True
+                    POOLS[pool]['info']['errors'].append(
+                        thread.exception)
             if not exceptions:
-                kwargs['investigator']['status'] = 'OK'
+                kwargs[pool]['status'] = 'OK'
+            return exceptions
+
+        map(populate_exceptions, POOLS.keys())
 
         resp.status = falcon.HTTP_200
         req.context['model'] = Status(**kwargs)

test/test_handlers_status.py
@@ -44,7 +44,7 @@ class Test_Status(TestCase):
         )
 
         # Make sure a Cluster is accepted as expected
-        status_model = status.Status(etcd={}, investigator={})
+        status_model = status.Status(etcd={}, investigator={}, clusterexec={})
         self.assertEquals(type(str()), type(status_model.to_json()))
 
 
@@ -53,7 +53,9 @@ class Test_StatusResource(TestCase):
     Tests for the Status resource.
     """
     astatus = ('{"etcd": {"status": "OK"}, "investigator": {"status": '
-               '"OK", "info": {"size": 1, "in_use": 1, "errors": []}}}')
+               '"OK", "info": {"size": 1, "in_use": 1, "errors": []}}, '
+               '"clusterexec": {"status": "OK", "info": '
+               '{"size": 1, "in_use": 1, "errors": []}}}')
 
     def before(self):
         self.api = falcon.API(middleware=[JSONify()])
@@ -72,11 +74,13 @@ class Test_StatusResource(TestCase):
         self.return_value._children = [child]
         self.return_value.leaves = self.return_value._children
 
-        POOLS['investigator'] = MagicMock(
-            'gevent.pool.Pool',
-            size=1,
-            free_count=lambda: 0,
-            greenlets=[])
+        for pool in ('investigator', 'clusterexec'):
+            POOLS[pool] = MagicMock(
+                'gevent.pool.Pool',
+                size=1,
+                free_count=lambda: 0,
+                greenlets=[])
+
         body = self.simulate_request('/api/v0/status')
         # datasource's get should have been called once
         self.assertEquals(1, self.datasource.get.call_count)

projectatomic/commissaire

src/commissaire/handlers/status.py
@@ -30,7 +30,7 @@ class Status(Model):
     """
     _json_type = dict
     _attributes = (
-        'etcd', 'investigator')
+        'etcd', 'investigator', 'clusterexecpool')
 
 
 class StatusResource(Resource):
@@ -58,6 +58,14 @@ class StatusResource(Resource):
                     'size': 0,
                     'in_use': 0,
                     'errors': [],
+                },
+            },
+            'clusterexecpool': {
+                'status': 'FAILED',
+                'info': {
+                    'size': 0,
+                    'in_use': 0,
+                    'errors': [],
                 }
             },
         }
@@ -70,21 +78,27 @@ class StatusResource(Resource):
         except etcd.EtcdKeyNotFound:
             kwargs['etcd']['status'] = 'FAILED'
 
-        # Check the investigator pool
-        kwargs['investigator']['info']['size'] = POOLS['investigator'].size
-        kwargs['investigator']['info']['in_use'] = (
-            POOLS['investigator'].size - POOLS['investigator'].free_count())
+        # Check all the pools
+        def populate_pool_info(pool):
+            # Append the pool information
+            kwargs[pool]['info']['size'] = POOLS[pool].size
+            kwargs[pool]['info']['in_use'] = (
+                POOLS[pool].size - POOLS[pool].free_count())
 
-        exceptions = False
-        for thread in POOLS['investigator'].greenlets:
-            if thread.exception:
-                exceptions = True
-                POOLS['investigator']['info']['errors'].append(
-                    thread.exception)
+        map(populate_pool_info, POOLS.keys())
 
-        if POOLS['investigator'].free_count() == 0:
+        def populate_exceptions(pool):
+            exceptions = False
+            for thread in POOLS[pool].greenlets:
+                if thread.exception:
+                    exceptions = True
+                    POOLS[pool]['info']['errors'].append(
+                        thread.exception)
             if not exceptions:
-                kwargs['investigator']['status'] = 'OK'
+                kwargs[pool]['status'] = 'OK'
+            return exceptions
+
+        map(populate_exceptions, POOLS.keys())
 
         resp.status = falcon.HTTP_200
         req.context['model'] = Status(**kwargs)

test/test_handlers_status.py
@@ -44,7 +44,7 @@ class Test_Status(TestCase):
         )
 
         # Make sure a Cluster is accepted as expected
-        status_model = status.Status(etcd={}, investigator={})
+        status_model = status.Status(etcd={}, investigator={}, clusterexecpool={})
         self.assertEquals(type(str()), type(status_model.to_json()))
 
 
@@ -53,7 +53,9 @@ class Test_StatusResource(TestCase):
     Tests for the Status resource.
     """
     astatus = ('{"etcd": {"status": "OK"}, "investigator": {"status": '
-               '"OK", "info": {"size": 1, "in_use": 1, "errors": []}}}')
+               '"OK", "info": {"size": 1, "in_use": 1, "errors": []}}, '
+               '"clusterexecpool": {"status": "OK", "info": '
+               '{"size": 1, "in_use": 1, "errors": []}}}')
 
     def before(self):
         self.api = falcon.API(middleware=[JSONify()])
@@ -72,11 +74,13 @@ class Test_StatusResource(TestCase):
         self.return_value._children = [child]
         self.return_value.leaves = self.return_value._children
 
-        POOLS['investigator'] = MagicMock(
-            'gevent.pool.Pool',
-            size=1,
-            free_count=lambda: 0,
-            greenlets=[])
+        for pool in ('investigator', 'clusterexecpool'):
+            POOLS[pool] = MagicMock(
+                'gevent.pool.Pool',
+                size=1,
+                free_count=lambda: 0,
+                greenlets=[])
+
         body = self.simulate_request('/api/v0/status')
         # datasource's get should have been called once
         self.assertEquals(1, self.datasource.get.call_count)