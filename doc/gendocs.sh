91b470c149190d5f523835fbf13f8f7446c324c2
Matthew Barnes <mbarnes@redhat.com>
2016-03-22 11:55:24 -0400

    gendocs.sh: Remove -M option from sphinx-apidoc
    The -M / --module-first option was added to sphinx-apidoc in version
    1.3 (released in March 2015), yet Fedora 23 still has version 1.2.x.
    
    We don't actually need the option so remove it to ease dependencies.

--- doc/gendocs.sh
+++ doc/gendocs.sh
@@ -2,5 +2,5 @@
 set -xeuo pipefail
 rm -rf _build
 rm -rf apidoc
-sphinx-apidoc -T -M -e -o apidoc ../src/commissaire/
+sphinx-apidoc -T -e -o apidoc ../src/commissaire/
 sphinx-build -b html . _build
